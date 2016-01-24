8fc7dc1041a6d2a7009c1d3f8f55d8ab2ecf1e39
Dana Gill <d.gill.design@gmail.com>
2016-01-24 16:01:56 -0500

    jq slide toggle

---
+++ public/scripts/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+// I found this here: http://stackoverflow.com/questions/15226140/expanding-menu-on-click
+	$(function() {
+	    $('.show-form').click(function(){
+	      $('.form-container').slideToggle();
+	    });
+	});
+
+});
